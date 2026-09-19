364d76f4628e1ddd9d5795ff8e222841840bf82b
Ahmedslh17 <ton_email_github@example.com>
2026-09-19 14:45:38 +0200

    Fix login GET request rendering

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -39,25 +39,32 @@ class PagesController < ApplicationController
 
   # Actions de connexion / déconnexion par formulaire
   def login
-  if request.post?
-    if params[:username] == ENV["ADMIN_USERNAME"] && params[:password] == ENV["ADMIN_PASSWORD"]
-      session[:admin_logged_in] = true
-      redirect_to admin_path
-    else
-      flash.now[:alert] = "Identifiants incorrects"
-      render :login, status: :unprocessable_entity
+    if request.post?
+      if params[:username] == ENV["ADMIN_USERNAME"] && params[:password] == ENV["ADMIN_PASSWORD"]
+        session[:admin_logged_in] = true
+        redirect_to admin_path
+      else
+        flash.now[:alert] = "Identifiants incorrects"
+        render :login, status: :unprocessable_entity
+      end
+    end
+    # Si c'est un GET, Rails affichera automatiquement la vue login.html.erb
+  end
+
+  def logout
+    session[:admin_logged_in] = nil
+    redirect_to root_path, notice: "Déconnecté !"
+  end
+
+  private
+
+  def authenticate_admin!
+    unless session[:admin_logged_in]
+      flash[:alert] = "Veuillez vous connecter."
+      redirect_to login_path
     end
   end
-end
 
-private
-
-def authenticate_admin!
-  unless session[:admin_logged_in]
-    flash[:alert] = "Veuillez vous connecter."
-    redirect_to login_path
-  end
-end
   def generate_csv(leads)
     CSV.generate(headers: true) do |csv|
       csv << ["ID", "Date", "Nom", "Email", "Telephone", "Choix / Services"]
