fc91430f3fae54d9121907af28cb89fdab267d27
Ahmedslh17 <ton_email_github@example.com>
2026-09-22 19:29:21 +0200

    Force RAILS_ENV=production for db:migrate

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,4 +5,6 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-bundle exec rails db:migrate
+
+# Forcer la création/mise à jour de la table sessions en production
+RAILS_ENV=production bundle exec rails db:migrate
